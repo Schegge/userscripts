3f4a599843eb8625969fb2c9a3efc693d8a60f6e
Schegge <sarazeni20@gmail.com>
2018-04-16 11:42:11 +0200

    fixes

--- EFP Controllo Automatico Storie Seguite.js
+++ EFP Controllo Automatico Storie Seguite.js
@@ -11,100 +11,103 @@
 
 
 (function($) {
-    function debugging(msg, variable) {
-        var messaggio = 'CSS:\t' + msg;
-        if (variable !== undefined) {
-            messaggio += ' (' + typeof variable + ') ';
-            console.log(messaggio, variable);
-            return;
-        }
-        console.log(messaggio);
-    }
-    debugging('inizio');
+   /*
+   function debugging(msg, variable) {
+       var messaggio = 'CSS:\t' + msg;
+       if (!variable) {
+           messaggio += ' (' + typeof variable + ') ';
+           console.log(messaggio, variable);
+           return;
+       }
+       console.log(messaggio);
+   }
+   */
+   // debugging('inizio');
 
-    // controllare se siamo sulla pagina delle storie seguite (followed) oppure no
-    var followPage = window.location.pathname === '/followed.php' ? true : false;
-    debugging('followPage', followPage);
+   // controllare se siamo sulla pagina delle storie seguite (followed) oppure no
+   var followPage = window.location.pathname === '/followed.php' ? true : false;
+   // debugging('followPage', followPage);
 
-    // se non siamo in followed recuperare le informazioni tramite load(), altrimenti non ce n'è bisogno
-    if (!followPage) {
-        $('body').append('<div id="data-followed" style="display: none"></div>'); // dove i dati verranno salvati
-        $('#data-followed').load('/followed.php #corpo', function() {
-            debugging('load /followed.php');
-            checkNewChapters('#data-followed');
-        });
-    } else {
-        checkNewChapters('#wrap');
-    }
+   // se non siamo in followed recuperare le informazioni tramite load(), altrimenti non ce n'è bisogno
+   if (!followPage) {
+      // dove i dati verranno salvati
+      $('body').append('<div id="data-followed" style="display: none"></div>');
+      $('#data-followed').load('/followed.php #corpo', function() {
+         // debugging('load /followed.php');
+         checkNewChapters('#data-followed');
+      });
+   } else {
+      checkNewChapters('#wrap');
+   }
 
-    function checkNewChapters(element) {
-        var save = {}; // nuovo controllo
-        // l'elenco delle storie seguite
-        $(element + ' #corpo > div:nth-child(1) > div:nth-child(3) > div')
-            .each(function() {
-                // titolo e capitolo si trovano in un <div> che non ha id
-                if ($(this).attr('id') === undefined) {
-                    // il titolo si trova nell'unico <strong> presente
-                    var title = $(this).children('strong').text().trim();
-                    // il capitolo in un <a>
-                    var chapter = $(this).children('a').text();
-                    // che combacia con (numeri).(spazio)(lettere)
-                    var found = chapter.match(/(\d+)\.\s\w+/);
-                    // found[1] combacia con il raggruppamento (\d+) ossia il numero del capitolo
-                    save[title] = parseInt(found[1]);
-                }
-            });
-        debugging('save', save);
+   function checkNewChapters(element) {
+      // nuovo controllo
+      var save = {};
+      // l'elenco delle storie seguite
+      $(element + ' #corpo > div:nth-child(1) > div:nth-child(3) > div').each(function() {
+         // titolo e capitolo si trovano in un <div> che non ha id
+         if (!$(this).attr('id')) {
+            // il titolo si trova nell'unico <strong> presente
+            var title = $(this).children('strong').text().trim();
+            // il capitolo in un <a>
+            var chapter = $(this).children('a').text();
+            // che combacia con (numeri).(spazio)(lettere)
+            var found = chapter.match(/(\d+)\.\s\w+/);
+            // found[1] combacia con il raggruppamento (\d+) ossia il numero del capitolo
+            save[title] = parseInt(found[1], 10);
+         }
+      });
+      // debugging('save', save);
 
-        var alert = ''; // servirà per indicare a quali storie sono stati aggiunti capitoli
-        // recuperare l'ultimo salvataggio
-        var past = JSON.parse(localStorage.getItem('followed'));
-        debugging('past', past);
+      // servirà per indicare a quali storie sono stati aggiunti capitoli
+      var alert = '';
+      // recuperare l'ultimo salvataggio
+      var past = JSON.parse(localStorage.getItem('followed'));
+      // debugging('past', past);
 
-        // se esiste controllare se ci sono modifiche, altrimenti fare il primo salvataggio
-        if (past) {
-            // per ogni elemento appena recuperato controllare con i dati salvati se ci sono state modifiche
-            for(var key in save) {
-                if (!save.hasOwnProperty(key) || !past.hasOwnProperty(key)) continue;
-                if (save[key] === past[key]) {
-                    debugging(key + ' non ha nuovi capitoli');
-                } else {
-                    // calcolare la differenza di capitoli
-                    var diff = save[key] - past[key];
-                    var testo = diff > 1 ? ' nuovi capitoli': ' nuovo capitolo';
-                    // aggiungerlo ad alert
-                    alert += '<b>' + key + '</b> ha ' + diff + testo + '.<br>';
-                    debugging(key + ' ha ' + diff + testo);
-                }
-
+      // se esiste controllare se ci sono modifiche, altrimenti fare il primo salvataggio
+      if (past) {
+         // per ogni elemento appena recuperato controllare con i dati salvati se ci sono state modifiche
+         for (var key in save) {
+            if (save.hasOwnProperty(key) && past.hasOwnProperty(key)) {
+               if (save[key] !== past[key]) {
+                  // calcolare la differenza di capitoli
+                  var diff = save[key] - past[key];
+                  var testo = diff > 1 ? ' nuovi capitoli' : ' nuovo capitolo';
+                  // aggiungerlo ad alert
+                  alert += '<b>' + key + '</b> ha ' + diff + testo + '.<br>';
+                  // debugging(key + ' ha ' + diff + testo);
+               } else {
+                  // debugging(key + ' non ha nuovi capitoli');
+               }
             }
-            // se siamo in followed salvare i nuovi cambiamenti
-            if (followPage) {
-                localStorage.setItem('followed', JSON.stringify(save));
-                debugging('salvati i nuovi cambiamenti');
-            }
-        } else {
+         }
+         // se siamo in followed salvare i nuovi cambiamenti
+         if (followPage) {
             localStorage.setItem('followed', JSON.stringify(save));
-            debugging("primo salvataggio");
-        }
+            // debugging('salvati i nuovi cambiamenti');
+         }
+      } else {
+         localStorage.setItem('followed', JSON.stringify(save));
+         // debugging('primo salvataggio');
+      }
 
-        // segnale per verificare se ci sono state modifiche oppure no, se ce sono state alert non è vuoto
-        var segnale = alert.length ? true : false;
-        if (!alert.length) alert += 'Nessun nuovo capitolo.';
-        debugging('segnale', segnale);
-        debugging('alert', alert);
+      // segnale per verificare se ci sono state modifiche oppure no, se ce sono state alert non è vuoto
+      var segnale = alert.length ? true : false;
+      if (!alert.length) alert += 'Nessun nuovo capitolo.';
+      // debugging('segnale', segnale);
+      // debugging('alert', alert);
 
-        // aggiungere la indicazione delle modifiche salvate in alert, e in homepage anche il cuoricino se ci sono state
-        if (followPage) {
-            $('#corpo > div:nth-child(1) > div:nth-child(3)').before('<div style="margin: 0px 10px 10px 15px; padding: 0 10px 10px 10px; color: #009999;">' + alert + '</div>');
-        } else if (segnale) {
-            var menuWidth = parseInt($('#secondmenu').width());
-            $('#secondmenu').prepend('<a id="cuore" style="color: #009999;" href="http://www.efpfanfic.net/followed.php">&#9825;</a><div id="alert" style="display: none; position: absolute; color: #009999; background-color: #fff; padding: 5px; font-size: 10px; line-height: 14px; text-align: left; box-shadow: 0 0 3px 0 rgba(91, 146, 178, .5); border-radius: 3px; margin-top: 5px; width: ' + menuWidth + 'px;">' + alert + '</div>'
-            );
-            $('#cuore').hover(function() {
-                $('#alert').slideToggle(400);
-            });
-        }
-    }
+      // aggiungere la indicazione delle modifiche salvate in alert, e in homepage anche il cuoricino se ci sono state
+      if (followPage) {
+         $('#corpo > div:nth-child(1) > div:nth-child(3)').before('<div style="margin: 0px 10px 10px 15px; padding: 0 10px 10px 10px; color: #009999;">' + alert + '</div>');
+      } else if (segnale) {
+         var menuWidth = parseInt($('#secondmenu').width(), 10);
+         $('#secondmenu').prepend('<a id="cuore" style="color: #009999;" href="http://www.efpfanfic.net/followed.php">&#9825;</a><div id="alert" style="display: none; position: absolute; color: #009999; background-color: #fff; padding: 5px; font-size: 10px; line-height: 14px; text-align: left; box-shadow: 0 0 3px 0 rgba(91, 146, 178, .5); border-radius: 3px; margin-top: 5px; width: ' + menuWidth + 'px;">' + alert + '</div>');
+         $('#cuore').hover(function() {
+            $('#alert').slideToggle(400);
+         });
+      }
+   }
 
 })(jQuery);
--- Viki Subtitles Settings.js
+++ Viki Subtitles Settings.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Viki: Subtitles Settings
 // @namespace    https://github.com/Schegge
-// @version      1.0.2
+// @version      1.0.3
 // @description  Change subtitles font, size, color, shadow and background
 // @author       Schegge
 // @include      https://www.viki.com/videos/*
@@ -20,15 +20,24 @@
 		width: '100'
 	};
 
-	var config = JSON.parse(JSON.stringify(defValues));
-
 	// saved values
-	var saved = localStorage.getItem('sub_style');
-	if (saved) {
-		saved = JSON.parse(saved);
-		for (var s in config) { // in case i'm going to change/add options
-			if (saved[s]) config[s] = saved[s];
-		}
+	var config = localStorage.getItem('sub_style');
+	if (config) {
+		config = JSON.parse(config);
+		// in case i'm adding options
+		/*
+			for (var d in defValues) {
+				if (!config[d]) config[d] = defValues[d];
+			}
+		*/
+		// in case i'm removing options
+		/*
+			for (var r in config) {
+				if (!defValues[r]) config[r] = undefined;
+			}
+		*/
+	} else {
+		config = JSON.parse(JSON.stringify(defValues));
 	}
 
 	// add css changes
@@ -38,47 +47,48 @@
 
 	// HTML for options
 	var options = document.createElement('div');
-	options.setAttribute('id', 'vs-options');
-	options.setAttribute('class', 'card card-content');
-	for (var c in config) {
+	options.id = 'vs-options';
+	options.className = 'card card-content';
+	Object.keys(config).forEach(function(c) {
 		options.innerHTML += '<div>' + c + ': <input id="vs-' + c + '" type="text" value="' + config[c] + '"></div>';
-	}
+	});
 	// the save button
 	var save = document.createElement('div');
-	save.setAttribute('id', 'vs-save');
-	save.setAttribute('class', 'btn-simple strong');
-	save.innerText = 'SAVE';
+	save.id = 'vs-save';
+	save.className = 'btn-simple strong';
+	save.textContent = 'SAVE';
 	options.appendChild(save);
 	// add options to the page
-	document.querySelector('.video-lists-tab-content').insertBefore(options, document.querySelector('.next-episode-card'));
+	document.querySelector('.video-lists-tab-content').insertAdjacentElement('afterbegin', options);
 
 	// save changes
 	save.addEventListener('click', function() {
-		for (var c in config) {
-			if (document.getElementById('vs-' + c).value.trim().length) {
+		Object.keys(config).forEach(function(c) {
+			if (document.getElementById('vs-' + c).value.trim()) {
 				config[c] = document.getElementById('vs-' + c).value;
 			} else {
 				config[c] = defValues[c];
 				document.getElementById('vs-' + c).value = config[c];
 			}
-		}
+		});
 		addCSS();
 		localStorage.setItem('sub_style', JSON.stringify(config));
-		save.innerText = 'SAVED';
-		setTimeout(function() { save.innerText = 'SAVE'; }, 1000);
+		save.textContent = 'SAVED';
+		setTimeout(function() { save.textContent = 'SAVE'; }, 1000);
 	}, false);
 
 	// add css function
 	function addCSS() {
-		style.innerHTML = '.vjs-text-track-display { bottom: 0px !important } ' +
+		var padding = (100 - parseInt(config.width, 10)) / 2;
+		style.textContent = '.vjs-text-track-display { bottom: 0px !important } ' +
 		'.vjs-fullscreen .vjs-text-track-display > div > div > div { ' +
 		'font-size: ' + config.size + ' !important; }' +
 		'.vjs-text-track-display > div > div { ' +
 		'font-size: ' + config.size + ' !important; ' +
 		'font-family: ' + config.font + ' !important; ' +
 		'top: auto !important;' +
-		'bottom: '+ config.bottom + ' !important; ' +
-		'padding: 0 ' + ((100 - parseInt(config.width)) / 2)  + '%; }' +
+		'bottom: ' + config.bottom + ' !important; ' +
+		'padding: 0 ' + padding + '%; }' +
 		'.vjs-text-track-display > div > div > div { ' +
 		'color: ' + config.color + ' !important;' +
 		'text-shadow:  -1px -1px 1px ' + config.shadow + ', 2px 2px 1px ' + config.shadow + ', 2px 2px 3px ' + config.shadow + ', 2px 2px 4px ' + config.shadow + ' !important;' +
